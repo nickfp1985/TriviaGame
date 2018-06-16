212fbc73a102c5977bcc033e7c70a4e184ecf31c
nickfp1985 <nfpeddie@gmail.com>
2018-06-16 09:20:33 -0700

    made some major changes to js after testing several timers, updated questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,83 @@
-window.onload = function() {
+$( document ).ready(function() {
+    $("#start").on("click", timer.start);
+    console.log( "ready!" );
+});
 
-    $("#start").click(triviaTimer.start);
+let intervalId;
+let clockRunning = false;
+let timer = {
+
+    time: 60
+    
+    reset: function() {
+    
+        timer.time = 60;
+    
+        // reset timer back to 60 seconds
+        $("#display").text("60");
+    },
+
+    start: function() {
+
+        // start the countdown and set the clock to running
+        if (!clockRunning) {
+            intervalId = setInterval(timer.count, 1000);
+            clockRunning = true;
+        }
+    },
+
+    stop: function() {
+        if (timer.time = 0) {
+            clearInterval(intervalId);
+            clockRunning = false;
+            alert('Your time is up!');
+        }
+        
+    },
+
+    count: function() {
+
+        // decrement timer by 1
+        timer.time--;
+
+        // get the new time
+        let time = timer.time;
+        console.log(time);
+
+        // show the remaining time in the display div
+        $("#display").text(time);
+    }
 };
 
-//  Variable that will hold our setInterval that runs the stopwatch
-let intervalId;
 
-// prevents timer from being speeding up
-let clockRunning = false;
-
-let triviaTimer = {
-
-time: 60
-reset: function() {
-
-    triviaTimer.time = 60;
-    // Change the "timer" div to "60."
-    $('#timer').text("60");
-},
-
-start: function() {
-
-    // start the clock
-    if (!clockRunning) {
-    clearInterval(invervalId);
-    intervalId = setInterval(triviaTimer.count, 1000);
-    triviaTimer.clockRunning = true;
-    }
-
-},
+// ************************************
+//  timer.start({countdown: true, startValues: {seconds: 60}}); //set the countdown to 60 seconds
+//  
+//  $('.countdown #counter').html(timer.getTimeValues().toString());
+//  
+//  timer.addEventListener('secondsUpdated', function (e) {
+//      $('.countdown #counter').html(timer.getTimeValues().toString());
+//  });
+//  timer.addEventListener('targetAchieved', function (e) {
+//      $('.countdown #counter').alert('Game Over!');
+//  });
+// ************************************
+//function countdown(minutes) {
+//    let seconds = 60;
+//    function tick() {
+//        
+//        let counter = document.getElementById("counter");
+//        seconds--;
+//        counter.text(seconds);
+//        if( seconds > 0 ) {
+//            setTimeout(tick, 1000);
+//        } else {
+//            if(mins > 1){
+//               countdown(mins-1);           
+//            }
+//        }
+//    }
+//    tick();
+//}
+//
+//$("#start").click(countdown);
